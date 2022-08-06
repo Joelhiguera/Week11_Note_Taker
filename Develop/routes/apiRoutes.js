4efae75ecbb32ccde8245b2787de91b1673aeac9
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-08-06 00:21:04 -0600

    add delete route in apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const { notes } = require("../db/note.json")
+const { id } = require('../db/note.json')
 const { v4:uuidv4 } = require("uuid");
 const { newNote } = require("../lib/notes");
 
@@ -16,6 +17,11 @@ router.post('/notes', (req, res) =>{
   req.body.id = uuidv4()
   res.json(newNote(req.body, notes))
   
+})
+
+router.delete('/:id', (req, res) => {
+  console.log('note deleted')
+  res.end()
 })
 
 module.exports = router
